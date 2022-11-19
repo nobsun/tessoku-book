3ac1a4da4c4ff88b97a2b3239e4173e264b18d91
Yamashita Nobuo <nobsun@sampou.org>
2022-11-19 22:58:20 +0900

    add template

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,14 @@
 module Main where
 
-import Lib
-
 main :: IO ()
-main = someFunc
+main = interact (encode . solve . decode)
+
+decode :: String -> [[Int]]
+decode =  map (map read . words) . lines
+
+encode :: [[Int]] -> String
+encode = unlines . map (unwords . map show)
+
+solve :: [[Int]] -> [[Int]]
+solve dds = case dds of
+    _ -> undefined
