0ec9b128faebdffb05b0287fb447a78bd6df1609
Yamashita Nobuo <nobsun@sampou.org>
2022-12-03 20:00:25 +0900

    start A09

---
+++ app/a09/Main.hs
@@ -0,0 +1,23 @@
+module Main where
+
+import qualified Data.ByteString.Char8 as B
+import Data.Maybe
+
+main :: IO ()
+main = B.interact (encode . solve . decode)
+
+decode :: B.ByteString -> [[Int]]
+decode =  map (map readInt . B.words) . B.lines
+
+readInt :: B.ByteString -> Int
+readInt = fst . fromJust . B.readInt
+
+encode :: [[Int]] -> B.ByteString
+encode = B.unlines . map (B.unwords . map showInt)
+
+showInt :: Int -> B.ByteString
+showInt = B.pack . show
+
+solve :: [[Int]] -> [[Int]]
+solve dds = case dds of
+    _ -> undefined
