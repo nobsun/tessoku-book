bcb894559fb816c8e0a9581ed2c03c5b14d6dd26
Yamashita Nobuo <nobsun@sampou.org>
2022-12-11 00:04:11 +0900

    add B14 Another Subset Sum

--- app/b14/Main.hs
+++ app/b14/Main.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import qualified Data.ByteString.Char8 as B
 import Data.Maybe ( fromJust )
 import Data.Array
 import Data.List
+import qualified Data.IntSet as Set
 
 main :: IO ()
 main = B.interact (encode . solve . decode)
@@ -14,12 +16,21 @@ decode =  map (map readInt . B.words) . B.lines
 readInt :: B.ByteString -> Int
 readInt = fst . fromJust . B.readInt
 
-encode :: [[Int]] -> B.ByteString
-encode = B.unlines . map (B.unwords . map showInt)
+encode :: [[B.ByteString]] -> B.ByteString
+encode = B.unlines . map B.unwords
 
 showInt :: Int -> B.ByteString
 showInt = B.pack . show
 
-solve :: [[Int]] -> [[Int]]
+solve :: [[Int]] -> [[B.ByteString]]
 solve dds = case dds of
-    _ -> undefined
+    [n,k]:as:_ -> [[judge n k as]]
+
+judge :: Int -> Int -> [Int] -> B.ByteString
+judge n k as
+    | any (`Set.member` ps) qs = "Yes"
+    | otherwise                = "No"
+    where
+        ps = Set.fromList [ sum s | s <- subsequences bs]
+        qs = [ k - sum s | s <- subsequences cs]
+        (bs,cs) = splitAt (n `div` 2) as
