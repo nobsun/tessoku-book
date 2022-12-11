efcd72c6a5531dcb9544a4bd4b60b736a7d9441a
Yamashita Nobuo <nobsun@sampou.org>
2022-12-11 20:59:48 +0900

    add A16 Dungeon 1

---
+++ app/a16/Main.hs
@@ -0,0 +1,33 @@
+module Main where
+
+import qualified Data.ByteString.Char8 as B
+import Data.Maybe ( fromJust )
+import Data.Array
+import Data.List
+
+main :: IO ()
+main = B.interact (encode . solve . decode)
+
+decode :: B.ByteString -> [[Int]]
+decode =  map (map readInt . B.words) . B.lines
+
+readInt :: B.ByteString -> Int
+readInt = fst . fromJust . B.readInt
+
+encode :: [[Int]] -> B.ByteString
+encode = B.unlines . map (B.unwords . map showInt)
+
+showInt :: Int -> B.ByteString
+showInt = B.pack . show
+
+solve :: [[Int]] -> [[Int]]
+solve dds = case dds of
+    [n]:as:bs:_ -> [[ count n as bs ]]
+
+count :: Int -> [Int] -> [Int] -> Int
+count n (a:as) bs = iter 0 a as bs
+    where
+        iter _ q [] _ = q
+        iter p q (a:as) (b:bs)
+            = iter q (min (a+q) (b+p)) as bs
+
