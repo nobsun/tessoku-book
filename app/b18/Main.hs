198e3cc8e2a2341c0220b4804b74c834088a06bf
Yamashita Nobuo <nobsun@sampou.org>
2022-12-16 09:47:09 +0900

    add B18 Subset Sum with Restoration

--- app/b18/Main.hs
+++ app/b18/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Control.Arrow
 import qualified Data.ByteString.Char8 as B
 import Data.Maybe ( fromJust )
 import Data.Array
@@ -22,4 +23,23 @@ showInt = B.pack . show
 
 solve :: [[Int]] -> [[Int]]
 solve dds = case dds of
-    _ -> undefined
+    [n,s]:as:_ -> sss s (zip as [1..])
+
+sss :: Int -> [(Int, Int)] -> [[Int]]
+sss s = iter [(0,[])]
+    where
+        iter _ [] = [[-1]]
+        iter bs ((a,i):ais)
+            = case dropWhile ((s <) . fst) (map ((a+)***(i:)) bs) of
+                [] -> iter bs ais
+                cs@((c,ps):_)
+                    | c == s    -> [length ps] : [reverse ps]
+                    | otherwise -> iter (cs >< bs) ais
+
+(><) :: Ord a => [(a,[b])] -> [(a,[b])] -> [(a,[b])]
+[] >< ys = ys
+xs >< [] = xs
+xxs@((x,is):xs) >< yys@((y,js):ys) = case compare x y of
+    LT -> (y,js) : (xxs >< ys)
+    EQ -> (x,is) : (xs >< ys)
+    GT -> (x,is) : (xs >< yys)
