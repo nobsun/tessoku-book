ec09177cefd013f95c94b0114fdf7a1670cb268c
Yamashita Nobuo <nobsun@sampou.org>
2022-12-28 23:33:08 +0900

    add B21 Longest Subpalindrome

--- app/b21/Main.hs
+++ app/b21/Main.hs
@@ -8,8 +8,8 @@ import Data.List
 main :: IO ()
 main = B.interact (encode . solve . decode)
 
-decode :: B.ByteString -> [[Int]]
-decode =  map (map readInt . B.words) . B.lines
+decode :: B.ByteString -> [[String]]
+decode =  map (map B.unpack . B.words) . B.lines
 
 readInt :: B.ByteString -> Int
 readInt = fst . fromJust . B.readInt
@@ -20,6 +20,32 @@ encode = B.unlines . map (B.unwords . map showInt)
 showInt :: Int -> B.ByteString
 showInt = B.pack . show
 
-solve :: [[Int]] -> [[Int]]
+solve :: [[String]] -> [[Int]]
 solve dds = case dds of
-    _ -> undefined
+    _:[s]:_ -> [[ size $ lcs s (reverse s) ]]
+
+lcs :: Eq a => [a] -> [a] -> Sized a
+lcs s t = head $ foldr phi (replicate (length s + 1) nil) t
+    where
+        phi y row = foldr (step y) [nil] (zip3 s row (tail row))
+        step y (x,cs1,cs2) row
+            | x == y     = (x `cons` cs2) : row
+            | otherwise  = cs1 `longer` head row : row
+
+type Sized a = ([a], Int)
+
+size :: Sized a -> Int
+size = snd
+
+nil :: Sized a
+nil = ([], 0)
+
+cons :: a -> Sized a -> Sized a
+cons x (xs, n) = (x : xs, n + 1)
+
+longer :: Sized a -> Sized a -> Sized a
+longer xs@(_,m) ys@(_,n)
+    | m < n     = ys
+    | otherwise = xs
+
+
