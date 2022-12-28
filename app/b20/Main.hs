9fe63d9c917c2d9f7dd0902527f8aa846e2db669
Yamashita Nobuo <nobsun@sampou.org>
2022-12-28 16:28:37 +0900

    revise B20

--- app/b20/Main.hs
+++ app/b20/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 module Main where
 
 import qualified Data.ByteString.Char8 as B
@@ -23,36 +24,14 @@ showInt = B.pack . show
 
 solve :: [[String]] -> [[Int]]
 solve dds = case dds of
-    [s]:[t]:_ -> [[ cost $ mce s t ]]
+    [s]:[t]:_ -> [[ mce s t ]]
 
-mce :: String -> String -> CostedOps
+mce :: String -> String -> Int
 mce xs = head . foldr phi row0 where
-    row0 = map ((,) <*> length) $ tails $ map Del xs
-    phi y row = foldr step [Ins y `cons` last row] xes where
+    row0 = [n, n-1 .. 0] where
+        n = length xs
+    phi !y row = foldr step [ 1 + last row] xes where
         xes = zip3 xs row (tail row)
-        step (x,es1,es2) row = h : row where
-            h | x == y    = Cpy x `cons` es2
-              | otherwise = minimumBy (comparing cost)
-                          [ Rep x y `cons` es2
-                          , Del x `cons` head row
-                          , Ins y `cons` es1 ]
-
-data Op
-    = Cpy Char
-    | Del Char
-    | Rep Char Char
-    | Ins Char
-    deriving (Eq, Show)
-
-type CostedOps = ([Op], Int)
-
-cost :: CostedOps -> Int
-cost = snd
-
-nil :: CostedOps
-nil = ([], 0)
-
-cons :: Op -> CostedOps -> CostedOps
-cons o (os, c) = case o of
-    Cpy _ -> (o:os, c)
-    _     -> (o:os, 1 + c)
+        step (!x,!es1,!es2) row = h : row where
+            h | x == y    = es2
+              | otherwise = 1 + minimum [es2, head row, es1]
