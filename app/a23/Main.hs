f5ac0e0f677df46c07959a3a1cb40893174544a6
Yamashita Nobuo <nobsun@sampou.org>
2022-12-31 00:50:50 +0900

    add A23 All Free

--- app/a23/Main.hs
+++ app/a23/Main.hs
@@ -1,8 +1,10 @@
 module Main where
 
+import Control.Arrow
 import qualified Data.ByteString.Char8 as B
 import Data.Maybe ( fromJust )
 import Data.Array
+import Data.Bits
 import Data.List
 
 main :: IO ()
@@ -22,4 +24,21 @@ showInt = B.pack . show
 
 solve :: [[Int]] -> [[Int]]
 solve dss = case dss of
-    _ -> undefined
+    [n,m]:bss -> [[mnc m n (map bin2int bss)]]
+
+mnc :: Int -> Int -> [Int] -> Int
+mnc m n as
+    | coupons > m = -1 
+    | otherwise   = coupons 
+    where
+        coupons = foldl' phi arr0 as ! ub
+        ub   = 2^n - 1
+        imp  = m + 1
+        arr0 = listArray (0, ub) $ 0 : replicate ub imp
+        phi arr a
+            = accumArray min imp (0, ub)
+            [ next prev | prev <- assocs arr
+                        , next <- [id, (a .|.) *** (1 +)]]
+
+bin2int :: [Int] -> Int
+bin2int = foldl' ((+) . (2 *)) 0
