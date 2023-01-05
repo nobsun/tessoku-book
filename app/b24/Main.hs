fceb2ff29bb62e5328431ca51e1dff63ea84aa12
Yamashita Nobuo <nobsun@sampou.org>
2023-01-06 01:55:03 +0900

    add B24 Many Boxes

--- app/b24/Main.hs
+++ app/b24/Main.hs
@@ -1,25 +1,81 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
 import qualified Data.ByteString.Char8 as B
 import Data.Maybe ( fromJust )
 import Data.Array
+import Data.Function
 import Data.List
+import Data.Ord
+import qualified Data.IntSet as S
 
 main :: IO ()
 main = B.interact (encode . solve . decode)
 
-decode :: B.ByteString -> [[Int]]
-decode =  map (map readInt . B.words) . B.lines
+solve :: [[Int]] -> [[Int]]
+solve dss = case dss of
+    [n]:xys -> [[lis as]]
+        where
+            as = concatMap (sortBy (flip compare) . map snd)
+               $ groupBy ((==) `on` fst)
+               $ sortBy (comparing fst)
+               $ map conv xys
+            conv [x,y] = (x,y)
+
+lis :: [Int] -> Int
+lis = pred . S.size . foldl phi (S.singleton 0)
+    where
+        phi s a = case S.lookupLE a s of
+            Just b
+                | b < a -> S.insert a (psi s a)
+            _           -> s
+        psi s a = case S.lookupGT a s of
+            Just c -> S.delete c s
+            _      -> s
+
+--
+
+class InterfaceForOJS a where
+    readB :: B.ByteString -> a
+    readBs :: B.ByteString -> [a]
+    readBs = map readB . B.words
+    decode :: B.ByteString -> [[a]]
+    decode = map readBs . B.lines
+
+    showB :: a -> B.ByteString
+    showBs :: [a] -> B.ByteString
+    showBs = B.unwords . map showB
+    encode :: [[a]] -> B.ByteString
+    encode = B.unlines . map showBs
+
+instance InterfaceForOJS Int where
+    readB = readInt
+    showB = showInt
+
+instance InterfaceForOJS String where
+    readB = readStr
+    showB = showStr
+
+instance InterfaceForOJS Double where
+    readB = readDbl
+    showB = showDbl
 
 readInt :: B.ByteString -> Int
 readInt = fst . fromJust . B.readInt
 
-encode :: [[Int]] -> B.ByteString
-encode = B.unlines . map (B.unwords . map showInt)
-
 showInt :: Int -> B.ByteString
 showInt = B.pack . show
 
-solve :: [[Int]] -> [[Int]]
-solve dss = case dss of
-    _ -> undefined
+readStr :: B.ByteString -> String
+readStr = B.unpack
+
+showStr :: String -> B.ByteString
+showStr = B.pack
+
+readDbl :: B.ByteString -> Double
+readDbl = read . B.unpack
+
+showDbl :: Double -> B.ByteString
+showDbl = B.pack . show
